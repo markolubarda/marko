e451fb98a3069d311b194122c30844c80326a128
marko lubarda <marko@ballab.com>
2017-10-23 13:47:11 +0200

    learning VueJs episode 10

--- jQuery/js/vueJsEpisode10_.js
+++ jQuery/js/vueJsEpisode10_.js
@@ -3,11 +3,20 @@
  */
 
 	// Episode 10
+Vue.component('modal', {
 
+	template: '<div class="modal is-active"><div class="modal-background"></div><div class="modal-content"><div class="box"><p>lorem ipsuem dolor sit amet and and and...</p></div></div><button class="modal-close is-large" aria-label="close"></button></div>'
+
+});
 
 
 var pro = new Vue({
 
 	el: '#episoed10',
+
+	data: {
+
+		showModal : false
+	}
 
 });
