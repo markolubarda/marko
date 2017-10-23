bb715cf6be9fe5125cc86bcbf12def4d69dcfa80
marko lubarda <marko@ballab.com>
2017-10-23 10:40:55 +0200

    learning VueJs2.html episode 8

--- jQuery/js/vueJsEpisode7_.js
+++ jQuery/js/vueJsEpisode7_.js
@@ -4,9 +4,32 @@
 
 Vue.component('task-list', {
 
+	template: '<div><task v-for="task in tasks">{{ task.task }}</task></div>',
+
+	data() {
+
+		return {
+
+			tasks: [
+
+				{ task: 'Go to the store', completed: true },
+
+				{ task: 'Finish screen cast', completed: false },
+
+				{ task: 'Make donation', completed: false },
+
+				{ task: 'Clean room', completed: true }
+			]
+		};
+	}
+
+});
+
+Vue.component('task', {
+
 	template: '<li><slot></slot></li>'
 
-})
+});
 
 
 var pro = new Vue({
