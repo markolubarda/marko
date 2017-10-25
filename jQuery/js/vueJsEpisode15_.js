d28f44ae077f482f9b06d82c2f1014ec64c09ad2
marko lubarda <marko@ballab.com>
2017-10-25 16:33:31 +0200

    learning VueJs episode 15

---
+++ jQuery/js/vueJsEpisode15_.js
@@ -0,0 +1,20 @@
+/**
+ * Created by markolubarda on 25.10.17.
+ */
+
+// Episode 15
+Vue.component('progress-view', {
+
+	data() {
+
+		return { completionRate: 0 };
+	}
+
+});
+
+
+var pro = new Vue ({
+
+	el: '#episode15',
+
+});
