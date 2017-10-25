af9d0cbb60cb2fd077a7254fa32cc9d057d53597
marko lubarda <marko@ballab.com>
2017-10-25 13:15:11 +0200

    learning VueJs episode 13

---
+++ jQuery/js/vueJsEpisode13_.js
@@ -0,0 +1,38 @@
+/**
+ * Created by markolubarda on 24.10.17.
+ */
+// Episode 13
+window.Event = new Vue();
+
+
+Vue.component('coupon', {
+
+	template: '<input placeholder="Enter your coupon code" @blur="onCouponApplied">',
+
+	methods: {
+
+		onCouponApplied() {
+
+			Event.$emit('applied');
+		}
+	}
+
+});
+
+
+var pro = new Vue ({
+
+	el: '#episode13',
+
+	data: {
+
+		couponApplied: false
+
+	},
+
+	created() {
+
+		Event.$on('applied', () => alert('Handling it!'));
+	}
+
+});
