4c3879f1d109fbec80411703f0ff071d24a3d8f4
marko lubarda <marko@ballab.com>
2017-10-31 14:11:39 +0100

    learning VueJs episode 17 (Hot Module Replacement)

---
+++ lesson/src/components/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+
+	<div>
+
+		<h2>The count is: {{ count }}</h2>
+
+		<p>
+			<button @click="count += 5">Increment (5)</button>
+		</p>
+
+	</div>
+
+</template>
+
+<script>
+
+	export default {
+
+		data() {
+
+			return { count: 0 };
+		}
+	}
+
+</script>
